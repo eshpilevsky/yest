c7d147459a65a5dd968e2f64a7cc671bccfe5168
Evgeny Shpilevsky <eshpilevsky@icloud.com>
2020-06-27 15:43:30 +0300

    Fix 4

---
+++ common/lib/map.js
@@ -0,0 +1,14 @@
+export const getAddresFromGeoobject = (geoObjects) => {
+  return getAddressFromString(geoObjects.get(0).properties.get('metaDataProperty').GeocoderMetaData.text)
+}
+
+export const getAddressFromString = (text) => {
+  return text.split(',').slice(2).join(',')
+}
+
+export const getAddresByCoords = async (ymaps, coords) => {
+  const myReverseGeocoder = await ymaps.geocode(coords)
+  const { geoObjects } = myReverseGeocoder
+  const address = getAddresFromGeoobject(geoObjects)
+  return address;
+}
---
+++ components/map-suggest/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="address-input">
+    <v-text-field
+      append-icon="close"
+      @click:append="appendIconCallback"
+      v-model="searchString"
+    >
+      <template v-slot:append-outer>
+        <v-btn outlined @click="onCancel">Отменить</v-btn>
+      </template>
+    </v-text-field>
+  </div>
+</template>
+<script>
+  import { mapActions, mapGetters, mapMutations } from 'vuex'
+  import { loadYmap } from 'vue-yandex-maps';
+  import { settings } from '@/plugins/ymapPlugin'
+
+  export default {
+    data: () => ({
+      searchString: ''
+    }),
+    async mounted () {
+      await loadYmap(settings);
+      this.addSuggestView(ymaps)
+    },
+    methods: {
+      ...mapMutations ('map', {
+        hideMap: 'HIDE_MAP',
+        switchToMapMode: 'UNSET_INPUT_ADDRESS_MODE'
+      }),
+      ...mapGetters('map', {
+        getCurrentCoords: 'getCurrentCoords',
+        getCurrentAddress: 'getCurrentAddress'
+      }),
+      addSuggestView (ymaps) {
+        var suggestView = new ymaps.SuggestView(this.$el.querySelector('input'))
+        suggestView.events.add('select', (e) => {
+          this.$emit('select', e)
+        })
+      },
+      appendIconCallback (e) {
+        this.searchString = ''
+      },
+      onCancel () {
+        if (this.getCurrentCoords().length === 0) {
+          this.hideMap()
+        }
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .v-text-field__slot ymaps {
+    color: black;
+  }
+  ymaps[class*=suggest-item_selected_yes] {
+    background-color: #00A646;
+  }
+</style>
