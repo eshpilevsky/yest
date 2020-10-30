8f122ff748e739f4c97e724916e8980d8e9d12f8
Denis <skadenis@mail.ru>
2020-10-30 16:28:14 +0300

    edit

--- store/search.js
+++ store/search.js
@@ -13,7 +13,7 @@ export const actions = {
     if((payload.search).length > 2){
       let answ = {};
       await ApiService.post('/search', payload).then((res)=>{
-        context.commit('SET_SEARCH_RES', res)
+        context.commit('SET_SEARCH_RES', res.data)
       }).catch((err)=>{
         console.error(err);
       });
