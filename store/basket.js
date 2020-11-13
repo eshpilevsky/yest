12e9e5b020f4532e98146939b429163f783fe20f
denis <skadenis@mail.ru>
2020-11-13 19:06:31 +0300

    fix error after 1 order

--- store/basket.js
+++ store/basket.js
@@ -246,7 +246,7 @@ export const mutations = {
 
     // Добавление товара в корзину в первый раз
     state.status = '200'
-    state.data.dishs.push(payload);
+    state.data.dishs = []
 
   },
 };
